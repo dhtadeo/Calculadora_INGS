dd2151ed2dcdbeacba75f15222fad0d03cf649ef
tadeo <69534046+dhtadeo@users.noreply.github.com>
2024-05-07 20:07:46 -0500

    Añadir pruebas (a medias)

--- collaborators.py
+++ collaborators.py
@@ -1,18 +1,26 @@
 import tkinter as tk
 
+# Al presionar botón de cerrar
 def close_window():
     root.destroy()
 
+# Crear ventana
 root = tk.Tk()
+root.resizable(False, False)
+root.geometry('300x150')
 root.title("Créditos")
 
+# Label 1
 label1 = tk.Label(root, text="- CALCULADORA -\n")
 label1.pack(pady=1)
 
-label2 = tk.Label(root, text="Santiago Emiliano Cheluja Martín\nLuis Alexis Martínez Rodríguez\nDiego Hernández Tadeo")
+# Label 2
+label2 = tk.Label(root, text="Santiago Emiliano Cheluja Martín\nLuis Alexis Martínez Rodríguez\nDiego Hernández Tadeo\n\n")
 label2.pack(pady=1)
 
+# Botón de cerrar
 close_button = tk.Button(root, text="Cerrar", command=close_window)
 close_button.pack()
 
+# Iniciar ventana
 root.mainloop()
--- initCalculator.py
+++ initCalculator.py
@@ -1,19 +1,27 @@
 import tkinter as tk
 import subprocess
 
+# Ejecutar classicCalculator
 def window_classicCalculator():
     subprocess.Popen(["python", "classicCalculator.py"])
 
+# Ejecutar collaborators
 def window_collaborators():
     subprocess.Popen(["python", "collaborators.py"])
 
+# Crear ventana
 root = tk.Tk()
 root.title("Menú")
+root.resizable(False, False)
+root.geometry('300x100')
 
-open_classicCalculator = tk.Button(root, text="Calculadora Clásica", command=window_classicCalculator)
+# Botón de abrir calculadora
+open_classicCalculator = tk.Button(root, text="Calculadora", command=window_classicCalculator)
 open_classicCalculator.pack(pady=10)
 
+# Botón de mostrar colaboradores
 open_collaborators = tk.Button(root, text="Créditos", command=window_collaborators)
 open_collaborators.pack(pady=10)
 
+# Iniciar programa
 root.mainloop()
